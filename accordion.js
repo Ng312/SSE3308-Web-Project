aa74bb60f44956e0a3b5af59da3ca21e7166296a
Ng312 <ngmt312@gmail.com>
2024-05-17 00:34:16 +0800

    added profile page & edit a bit

--- accordion.js
+++ accordion.js
@@ -1,18 +1,27 @@
 const accordions = document.querySelectorAll('.accordion');
 
 accordions.forEach(accordion => {
+    const question = accordion.querySelector('.question');
     const arrow = accordion.querySelector('.arrow');
     const answer = accordion.querySelector('.answer');
 
-    accordion.addEventListener('click', () => {
+    question.addEventListener('click', () => {
+        const isActive = accordion.classList.contains('active');
         
-        if(arrow.classList.contains('active')) {
-            arrow.classList.remove('active');
-            answer.style.maxHeight = null;
-        } else {
+        accordions.forEach(acc => {
+            acc.classList.remove('active');
+            acc.querySelector('.arrow').classList.remove('active');
+            acc.querySelector('.answer').style.maxHeight = null;
+        });
+        
+        if (!isActive) {
+            accordion.classList.add('active');
             arrow.classList.add('active');
             answer.style.maxHeight = answer.scrollHeight + 'px';
+        } else {
+            accordion.classList.remove('active');
+            arrow.classList.remove('active');
+            answer.style.maxHeight = null;
         }
-        
-    })
-})
+    });
+});
