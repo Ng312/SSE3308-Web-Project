d4fd2bde6e990ece75b8c831707c699cd9457cd8
Ng312 <ngmt312@gmail.com>
2024-05-17 09:44:12 +0800

    added cart.html

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
-            });
-        }
-    });
+    // Apply smooth scrolling only to the "Contact Us" link
+    $('a[href="#contact-us"]').on('click', function(event) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+            window.location.hash = hash;
+        });
+    })
 });
